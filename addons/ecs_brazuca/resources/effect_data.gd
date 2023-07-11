84a5d9843702dcba8443d0f274d15ec4ec3d5e99
Raphael Gramosa <rafagramosa100@gmail.com>
2023-07-11 04:03:37 -0300

    Made EffectData class more responsible in editor and simplified part of the code

--- addons/ecs_brazuca/resources/effect_data.gd
+++ addons/ecs_brazuca/resources/effect_data.gd
@@ -8,45 +8,65 @@ class_name EffectData
 ## pseudo CONTINUOUS_HEALING would be damage < 0
 ## pseudo VELOCITY_BUFF would be velocity_ratio > 1
 ## pseudo DAMAGE_BUFF would be damage_change_ratio > 1
-enum EffectTypes {NOTHING=0, CONTINUOUS_DAMAGE=1, STUN=2, DAMAGE_DEBUFF=3, RESISTENCE_DEBUFF=4}
+enum EFFECT_TYPES {NOTHING=0, CONTINUOUS_DAMAGE=1, VELOCITY_DEBUFF=2, DAMAGE_DEBUFF=3, RESISTENCE_DEBUFF=4}
 
 ## Fixed structures to each effect type, "any" are keys common to every effect
 ## MUST add a new structure to each EffectType
-const effects_structures: Dictionary = {
-	"any": {
-		"duraction": 0.0
+const EFFECTS_STRUCTURES: Dictionary = {
+	EFFECT_TYPES.NOTHING: {
+		# Well its really nothing
 	},
-	EffectTypes.CONTINUOUS_DAMAGE: {
-		"damage_per_second": 1
+	EFFECT_TYPES.CONTINUOUS_DAMAGE: {
+		"damage_per_second": 1,
 	},
-	EffectTypes.STUN: {
+	EFFECT_TYPES.VELOCITY_DEBUFF: {
 		"valocity_ratio": 0.0
 	},
-	EffectTypes.DAMAGE_DEBUFF: {
-		"damage_change_ratio": 0.0
+	EFFECT_TYPES.DAMAGE_DEBUFF: {
+		"damage_ratio": 0.0
 	},
-	EffectTypes.RESISTENCE_DEBUFF: {
-		"resistence_change_ratio": 0.0
+	EFFECT_TYPES.RESISTENCE_DEBUFF: {
+		"resistence_ratio": 0.0
+	}
+}
+
+const EFFECTS_TARGETS: Dictionary = {
+	EFFECT_TYPES.CONTINUOUS_DAMAGE: {
+		"component_target_group": "HealthComponentGroup",
+		"properties": null
 	}
 }
 
 ## The specific name of the effect, just for visual behaviour and/or organization, like Burn, Freeze and etc... 
-@export_placeholder("The name of the effect") var effect_name: String
+@export_placeholder("Effect Name") var effect_name: String
 
-## Chose one of the existents effects, expect for NOTHING, because well... its does nothing
-@export var effect_type: EffectTypes = EffectTypes.NOTHING:
+# Mental note: EFFECT_TYPES and effect_type are different, effect_type are just one of the possibles EFFECT_TYPES (Isso é obvio mas continuo me confundindo)
+## Chose one of the existents effects, except for NOTHING, because well... its does nothing
+@export var effect_type: EFFECT_TYPES = EFFECT_TYPES.NOTHING:
 	set(new_effect_type):
-		#Does not change nothing if the values are the same
+		# Does not change nothing if the values are the same
 		if effect_type == new_effect_type:
 			return
-			
-		# Add the base data, to every effect type, no matter what
-		effects_data = effects_structures["any"].duplicate()
-		# Merge the base dictionary with the correct structure
-		effects_data.merge(effects_structures[new_effect_type].duplicate())
 		
 		effect_type = new_effect_type
+		
+		## Modify effects_data
+		# Give the specific dictionary structure to effects_data
+		effects_data = EFFECTS_STRUCTURES[effect_type].duplicate()
 
-"""Mudar no futuro para tornar impossível mudar a estrutura esperada do dicionário"""
-## Change the values according, does NOT add new keys or change values types
-@export var effects_data: Dictionary
+## The duration of the effect, if 0 the effect will not be applied, if less than 0 the effect will be considered permanent.
+## A permanent effect are NOT removed automatically by the system with a cooldown
+@export var effect_duraction: float = 0.0
+
+## Change the values according, does NOT change values types. You can not add new keys, even if you try
+## Beware with ratio properties, because it can become a very low or high values.
+## For example a damage of 1000 with damage_ratio of 1000 become 1,000,000
+@export var effects_data: Dictionary:
+	set(new_effects_data):
+		# Verify if the keys from new_effects_data does not match with the structure described by
+		# EFFECTS_STRUCTURES based on the given effect_type
+		if new_effects_data.keys() != EFFECTS_STRUCTURES[effect_type].keys():
+			push_warning("You cannot change the structure of the dictionary effects_data, each effect have a specific structure, that was not developed to be changed from editor")
+			return
+		
+		effects_data = new_effects_data

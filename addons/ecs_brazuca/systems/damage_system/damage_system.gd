9b0b03e304a33981087c43cd0de9c3055ce122c5
Raphael Gramosa <rafagramosa100@gmail.com>
2023-08-27 04:27:24 -0300

    Added the possibility to consider (or ignore) the resistance_ratio of the HealthComponent and the damage_ratio of DamageComponent. Its functional, but not much userfull until EffectSystem and related classes be 100% working

--- addons/ecs_brazuca/systems/damage_system/damage_system.gd
+++ addons/ecs_brazuca/systems/damage_system/damage_system.gd
@@ -1,9 +1,7 @@
+@icon("damage_system_icon.svg")
 extends BaseSystem
 
 class_name DamageSystem
-
-func get_class_name():
-	return "DamageSystem"
 
 func _init() -> void:
 	super()
@@ -12,7 +10,7 @@ func _init() -> void:
 
 ## if oposite_behaviour be true, the damage will be multiplied by -1. So damage become a healing and vice-versa
 ## specific_component_name only is necessary if the entity have two or more componenets from the same group
-func do_damage(source_entity: Node, target_entity: Node, oposite_behaviour: bool = false, source_component_name: String = "", target_component_name: String = "") -> void:
+func do_damage(source_entity: Node, target_entity: Node, oposite_behaviour: bool = false, true_damage: bool = false, source_component_name: String = "", target_component_name: String = "") -> void:
 	
 	# The source_entity must have at least one component from DamageComponentGroup, and target_entity at least one HealthComponent
 	var target_component = get_component_from_entity(target_entity, "HealthComponentGroup", target_component_name)
@@ -23,18 +21,24 @@ func do_damage(source_entity: Node, target_entity: Node, oposite_behaviour: bool
 	if source_component == null:
 		return
 	
-	var real_damage = _get_real_damage(source_component, target_component)
+	var real_damage = _get_real_damage(source_component, target_component, true_damage)
 	
 	if oposite_behaviour == false:
 		target_component.update_health(real_damage)
 	else:
 		target_component.update_health(-real_damage)
-
+	
+	
 func do_continuous_damage(source_entity: Node, target_entity: Node) -> void:
 	pass
 
-func _get_real_damage(damage_component: DamageComponent, health_component: HealthComponent):
+func _get_real_damage(damage_component: DamageComponent, health_component: HealthComponent, true_damage: bool):
 	var base_damage: int = damage_component.get_damage()
-	var real_damage: int = int(base_damage * (damage_component.get_damage_ratio() / health_component.get_resistance_ratio()))
+	var real_damage: int
+	# The true damage ignore the resistance and damage_ratio
+	if true_damage == true:
+		real_damage = base_damage
+	else:
+		real_damage = int(base_damage * (damage_component.get_damage_ratio() / health_component.get_resistance_ratio()))
 	
 	return real_damage
